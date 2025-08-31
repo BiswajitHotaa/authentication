bf0923cb4aa5a1c743580661c1b88b36930e898c
Biswa <biswajithotaa@gmail.com>
2025-08-31 23:59:17 +0530

    jira-104 resolved

--- springboot-auth-starter/src/main/java/com/example/auth/AuthApplication.java
+++ springboot-auth-starter/src/main/java/com/example/auth/AuthApplication.java
@@ -4,15 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthApplication {
+public class AuthApplication 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
 
 //102
 public void m2()
 {
 	//logic
 }
-
-    public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
-    }
 }
