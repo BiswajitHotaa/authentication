50f24e6237e78780e2f35767c543d205862a6baf
Biswa <biswajithotaa@gmail.com>
2025-08-31 23:53:12 +0530

    JIRA-102 solved

--- springboot-auth-starter/src/main/java/com/example/auth/AuthApplication.java
+++ springboot-auth-starter/src/main/java/com/example/auth/AuthApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthApplication {
+
+//102
+public void m2()
+{
+	//logic
+}
+
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
     }
